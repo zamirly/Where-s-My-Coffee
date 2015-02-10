f036c829f27549dacc10b94d86c240831b53bb4e
zamirly <tal.zamirly@gmail.com>
2015-02-10 13:28:18 +1100

    Initial Commit

---
+++ Where's My Coffee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Where's My Coffee
+//
+//  Created by Tal Zamirly on 10/02/15.
+//  Copyright (c) 2015 Tal Zamirly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Where's My Coffee/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Where's My Coffee
+//
+//  Created by Tal Zamirly on 10/02/15.
+//  Copyright (c) 2015 Tal Zamirly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
